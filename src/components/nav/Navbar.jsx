84ea185f9b63c57b6b24976d2d8c9873448ca875
DanGraziano <dangraziano@outlook.com>
2024-01-04 22:17:35 -0500

    Add dark/light mode logos

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,19 +1,23 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { FaMoon, FaSun } from "react-icons/fa";
 
 export default function Navigation() {
     return (
-        <Navbar expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
+        <Navbar expand="lg" className="bg-black" variant="dark">
           <Container>
             <Navbar.Brand href="#home">DG</Navbar.Brand> 
             {/* TODO - replace DG with my picture or fancy text */}
+            <Nav.Link href=""><FaMoon /></Nav.Link>
+            <Nav.Link href=""><FaSun /></Nav.Link>
+            {/* TODO - implement dark/light modes */}
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="me-auto">
               </Nav>
               <Nav>
-              <Nav.Link href="#about">About</Nav.Link>
+                <Nav.Link href="#about">About</Nav.Link>
                 <Nav.Link href="#projects">Projects</Nav.Link>
                 <Nav.Link href="#work">Work</Nav.Link>
                 <Nav.Link href="#contact">Contact</Nav.Link>
