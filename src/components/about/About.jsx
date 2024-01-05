15c920dcfe3a7f2341949618bd5b737ab2a31b54
DanGraziano <dangraziano@outlook.com>
2024-01-04 22:32:40 -0500

    Add basic content for about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,13 +1,29 @@
-
-
-
 export default function About() {
     return (
-        <div className="about">
-            <h1>About</h1>
+        <div className="about-container-fluid">
+            <div className="row justify-content-center">
+                <div className="col-12 col-md-8 col-lg-6">
+                    <h1 className="about-section-title text-center mt-5">
+                        <span className="about-section-title-color">About</span>
+                    </h1>
+                </div>
+            </div>
+            <div className="row justify-content-center">
+                <div className="col-12 col-md-8 col-lg-6">
+                    <div className="about-section-description text-center">
+                        <p className="about-section-description-text m-5">
+                            I am in the final semester of earning my Master's degree in computer science from Northeastern University. 
+                            I'm currently seeking in-person, hybrid, or remote software engineering opportunities. 
+                        </p>
+                        <p className="about-section-description-text m-5">
+                            I am a former journalist and public relations professional with over a decade of experience in the technology industry. 
+                            As a journalist, I had the privilege of writing about new products and technologies and sharing them with millions of readers. 
+                            During my time in public relations, I played a crucial role in successfully launching dozens of new products and innovative technologies. 
+                            Now, as I work towards completing my Master's degree, I am eager to contribute to building the next generation of technology products.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
-
-
-
-}
+};
