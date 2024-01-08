967340f8c2f6ab8d641b34f60d3e4dc2a0069235
DanGraziano <dangraziano@outlook.com>
2024-01-08 00:29:21 -0500

    Add styling for hero section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Container, Row, Col, Image, Button } from 'react-bootstrap';
-import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import { FcGlobe } from "react-icons/fc";
 import { MdOutlineSchool } from "react-icons/md";
 import Logo from '../../assets/dan-photo.jpg';
 import { Element, Link } from 'react-scroll';
+import './Hero.css';
+import {ReactComponent as LinkedIn} from '../../assets/linkedin.svg';
+import {ReactComponent as Github} from '../../assets/github.svg';
+
 
 export default function Hero() {
     return (
@@ -12,22 +15,29 @@ export default function Hero() {
             <Container className="hero-container">
                 <Row className="justify-content-center align-items-center">
         
-                    <Col xs={12} md={4} className="text-center">
-                        <img src={Logo} height={230} width={230} alt="Dan Graziano" className="rounded-circle my-4" />
+                    <Col className="text-center image-column mb-3">
+                        <img src={Logo} height={230} width={230} alt="Dan Graziano" className="rounded-circle" />
                     </Col>
+                </Row>
 
-                    <Col xs={6} md={4}>
+                <Row className="justify-content-center">
+                    
+                    <Col className="text-column">
                         <h1 className="hello-text">Hi, I'm Dan.</h1>
                         <h5 className="sub-hed-text">I'm a Full Stack Software Engineer.</h5>
-                        <div className="location-text d-flex align-items-center mt-3"><FcGlobe /> Portland, ME</div> 
-                        <div className="education-text d-flex align-items-center my-1"><MdOutlineSchool /> MSCS, Northeastern University</div> 
-                        <div className="education-text d-flex align-items-center my-1"><MdOutlineSchool /> BA, Sacred Heart University</div> 
-                        <a href="https://github.com/DanGraziano" className=""><FaGithub /></a>
-                        <a href="https://www.linkedin.com/in/danielgraziano/" className="m-3"><FaLinkedinIn /></a>
+                        <div className="location-text mt-3 text-center"><FcGlobe /> Portland, ME</div> 
+                        <div className="education-text my-1 text-center" ><MdOutlineSchool /> MSCS, Northeastern University</div> 
+                        <div className="education-text text-center"><MdOutlineSchool /> BA, Sacred Heart University</div> 
                     </Col>
-
+                </Row>
+                    <Row>
+                        <div className="icon-container text-center my-2">
+                            <a href="https://github.com/DanGraziano" className ="logo" ><Github /></a>
+                            <a href="https://www.linkedin.com/in/danielgraziano/" className="logo"><LinkedIn /></a>
+                        </div>
+                    </Row>
                     <Row className="justify-content-center">
-                        <Col className="text-center hero-section-button my-4" >
+                        <Col className="text-center hero-section-button" >
                             <Link
                                 activeClass="navbar-active-link"
                                 spy={true}
@@ -36,10 +46,9 @@ export default function Hero() {
                                 duration={500}
                                 to="contact"
                             >
-                            <Button variant="primary mb-4 mt-3 mb-lg-0">Reach out</Button>
+                            <Button variant="primary reach-out-button">Reach out</Button>
                             </Link>
                         </Col>
-                </Row>
                 </Row>
             </Container>
         </Element>
